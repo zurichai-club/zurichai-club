1aae511e08165bb7c43aabe5e15f8dba1a9ee244
Michał Koźmiski <michal@kozminski.co>
2026-07-21 21:51:26 +0200

    Render sponsor presentation as page

--- src/pages/partners_pres.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-import presentation from "../../meetup_sponsor_slides.html?raw";
-
-export const GET: APIRoute = () =>
-  new Response(presentation, {
-    headers: {
-      "Content-Type": "text/html; charset=utf-8"
-    }
-  });
